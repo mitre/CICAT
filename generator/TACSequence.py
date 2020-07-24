8a6e925402906cde65eef40f9936e494fba37a1c
Jackson E Wynn <jwynn@mitre.org>
2020-07-24 10:48:03 -0400

    Replace TACSequence.py

--- generator/TACSequence.py
+++ generator/TACSequence.py
@@ -1,36 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-:::::::::::::::::::::::::::::::::::::  MITRE CRP PROJECT  :::::::::::::::::::::::::::::::::::::::
-      
+::::::::::::::::::::::::  Critical Infrastructure Cyberspace Analysis Tool (CICAT)  :::::::::::::::::::::::::::::::::::::::
+
                                             NOTICE
+                                            
+The contents of this material reflect the views of the author and/or the Director of the Center for Advanced Aviation 
+System Development (CAASD), and do not necessarily reflect the views of the Federal Aviation Administration (FAA) 
+or the Department of Transportation (DOT). Neither the FAA nor the DOT makes any warranty or guarantee, or promise, 
+expressed or implied, concerning the content or accuracy of the views expressed herein. 
 
-This software (or technical data) was produced for the U. S. Government under contract 355358
-with Brookhaven National Laboratory, and is subject to the Rights in Data-General Clause 52.227-14 (MAY 2014) or (DEC 2007).
+This is the copyright work of The MITRE Corporation and was produced for the U.S. Government under Contract Number 
+DTFAWA-10-C-00080 and is subject to Federal Aviation Administration Acquisition Management System Clause 3.5-13, 
+Rights in Data-General, Alt. III and Alt. IV (Oct. 1996). No other use other than that granted to the U.S. Government, 
+or to those acting on behalf of the U.S. Government, under that Clause is authorized without the express written permission 
+of The MITRE Corporation. For further information, please contact The MITRE Corporation, Contract Office, 7515 Colshire Drive, 
+McLean, VA 22102 (703) 983-6000. ©2020 The MITRE Corporation. 
 
-The following copyright notice may be affixed after receipt of written approval from the Contracting Officer.
-Please contact the Contracts Management Office for assistance with obtaining approval or identifying the correct clause.
-If the contract has Clause 52.227-14, Alt. IV, written approval is not required and the below copyright notice may be affixed.
-
+The Government retains a nonexclusive, royalty-free right to publish or reproduce this document, or to allow others to do so, for 
+“Government Purposes Only.”                                           
+                                            
 (c) 2020 The MITRE Corporation. All Rights Reserved.
 
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+TACSequence.py - Routines to implement tactic patterns and generate TTP sequences
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 """
 
 import sys
 from random import sample
 from collections import defaultdict
-from ffactory import FILTER_FACTORY, INIT_FILTERS #, getTTPs, showTTPs
-from loaddata import LOAD_DATA, LOAD_TTP_SUPPLEMENT #, LOAD_ATK4ICS, LOAD_ACTOR_PROFILES 
- 
-from topology import INIT_TOPOLOGY #, m_topology, m_zoneCIs, m_zoneMMap
-from loaddata import m_file_INFRASTRUCTURE, m_file_SCENARIOS, m_file_ODNI #m_file_EXTENSIONS, 
+
+from ffactory import FILTER_FACTORY, INIT_FILTERS
+from loaddata import LOAD_DATA, LOAD_TTP_SUPPLEMENT
+from topology import INIT_TOPOLOGY
+from loaddata import m_file_INFRASTRUCTURE, m_file_SCENARIOS, m_file_ODNI 
 from TTPFilter import TTP_FILTER
-
-#from stats import TAGS_INDEX, x_TACTICS 
-
-#from mitGEN import ACTOR_REPORT
 import ODNI
-
 
 # ATT&CK tactics list
 m_TACTIC_LIST = ['initial-access', 'execution', 'persistence', 'privilege-escalation', 'defense-evasion', 'credential-access', 
