244e8001b3ab01acdd30294b0ec7eb5a3830445b
Jackson E Wynn <jwynn@mitre.org>
2020-07-24 10:37:04 -0400

    Replace ODNI.py

--- generator/ODNI.py
+++ generator/ODNI.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-:::::::::::::::::::::::::::::::::::::  MITRE CRP PROJECT  :::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::  Critical Infrastructure Cyberspace Analysis Tool (CICAT)  :::::::::::::::::::::::::::::::::::::::
 
                                             NOTICE
+                                            
+The contents of this material reflect the views of the author and/or the Director of the Center for Advanced Aviation 
+System Development (CAASD), and do not necessarily reflect the views of the Federal Aviation Administration (FAA) 
+or the Department of Transportation (DOT). Neither the FAA nor the DOT makes any warranty or guarantee, or promise, 
+expressed or implied, concerning the content or accuracy of the views expressed herein. 
 
-This software (or technical data) was produced for the U. S. Government under contract 355358
-with Brookhaven National Laboratory, and is subject to the Rights in Data-General Clause 52.227-14 (MAY 2014) or (DEC 2007).
+This is the copyright work of The MITRE Corporation and was produced for the U.S. Government under Contract Number 
+DTFAWA-10-C-00080 and is subject to Federal Aviation Administration Acquisition Management System Clause 3.5-13, 
+Rights in Data-General, Alt. III and Alt. IV (Oct. 1996). No other use other than that granted to the U.S. Government, 
+or to those acting on behalf of the U.S. Government, under that Clause is authorized without the express written permission 
+of The MITRE Corporation. For further information, please contact The MITRE Corporation, Contract Office, 7515 Colshire Drive, 
+McLean, VA 22102 (703) 983-6000. ©2020 The MITRE Corporation. 
 
-The following copyright notice may be affixed after receipt of written approval from the Contracting Officer.
-Please contact the Contracts Management Office for assistance with obtaining approval or identifying the correct clause.
-If the contract has Clause 52.227-14, Alt. IV, written approval is not required and the below copyright notice may be affixed.
-
+The Government retains a nonexclusive, royalty-free right to publish or reproduce this document, or to allow others to do so, for 
+“Government Purposes Only.”                                           
+                                            
 (c) 2020 The MITRE Corporation. All Rights Reserved.
 
-
-ODNI.py - Implements ODNI cyber framework
-
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ODNI.py - Constructs ODNI stages and objectives and maps ATT&CK tactics and techniques
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 """
+
 
 import openpyxl
 import random
-#from collections import defaultdict #, OrderedDict
 
 class STAGE():
     def __init__ (self, name, desc):
@@ -278,7 +285,7 @@ def success(val):
         return False   
     return True
 
-def nextstep(listx, step):#, pSuc):
+def nextstep(listx, step):
     if (step < len(listx)-1):
        return step + 1
 
@@ -292,7 +299,7 @@ def findObjectiveByName( name):
 
 
 def printTTP (ttp):
-    print ('\n' + ttp.getTECHID(), ':', ttp.getName() ) #, '-', ttp.getDesc() )
+    print ('\n' + ttp.getTECHID(), ':', ttp.getName() ) 
     print ('URL:', ttp.getURL() )
     print ('Platform(s):', ttp.getPlatform(), 'Tactic[s]:', ttp.getTactic() )
     print ('Tag(s):', ttp.getTags())
