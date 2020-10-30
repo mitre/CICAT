052e3e14e1fffdfb6cd2a8d01e9aa45e56cb233d
jwynn <jwynn@mitre.org>
2020-10-30 08:18:52 -0400

    Add files via upload

--- cicat/example/CCATrun.bat
+++ cicat/example/CCATrun.bat
@@ -1,1 +1,3 @@
+rem cd C:\Projects\FY20\CICAT\CICATv1.1_081120_2\faa-cicat-master\generator
 
+python ..\generator\scenGEN.py -r -i INFRASTRUCTURE.ap1000.xlsx -s SCENARIOS.ap1000.xlsx -o RESULTS.ap1000.xlsx
